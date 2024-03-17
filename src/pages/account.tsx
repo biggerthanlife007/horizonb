3206ab5751513038d9563cd94606853a17c290bb
heiswillz98 <wheeliams98@gmail.com>
2024-03-17 20:26:12 +0100

    updated transactions

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -146,9 +146,9 @@ const Home = () => {
                       <TransactionItem
                         date="March 17, 2024"
                         company="Chase Bank"
-                        description="Direct Deposit"
+                        description="Failed"
                         amount={"200.00"}
-                        transactionType="pending"
+                        transactionType="debit"
                       />
                       <TransactionItem
                         date="March 17, 2024"
